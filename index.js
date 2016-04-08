47532ef935118509e716975e2a71580fbcd03cfd
Xiphe <github@xiphe.net>
2016-04-08 16:53:53 +0200

    feat: implement

---
+++ index.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const getChangelogrcConfig = require('get-changelogrc-config');
+const getPrompts = require('./lib/prompts');
+const formatCommitMsg = require('./lib/formatCommitMsg');
+const changelogrcAngular = require('changelogrc-angular');
+const log = require('./lib/log');
+
+module.exports = {
+  prompter(inquirer, commit) {
+    getChangelogrcConfig()
+      .catch(() => changelogrcAngular)
+      .then((config) => {
+        const prompts = getPrompts(config);
+
+        return inquirer.prompt(prompts, (answers) => {
+          const message = formatCommitMsg(answers, config);
+
+          if (message) {
+            return commit(message);
+          }
+
+          return log.warn('nothing committed');
+        });
+      })
+      .catch(err => log.error(err));
+  },
+};
